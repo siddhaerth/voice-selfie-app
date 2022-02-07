28ac2890830ed9525d9288e61b193b22ad66b4b1
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2022-02-07 14:33:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,14 +13,21 @@ recognition.onresult = function (event) {
     speak();
 
     document.getElementById("textbox").innerHTML = Content;
+    if(Content=="take my selfie"){
+        console.log("taking selfie ---");
+        speak();
+    }
 }
 
 function speak() {
 var synth = window.speechSynthesis;
-speak_data= document.getElementById("textbox").value;
+speak_data= "Taking your selfie in 5 seconds";
 var utterThis= new SpeechSynthesisUtterance(speak_data);
 synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout(function(){
+    take_snapshot();
+},5000);
 }
 
 Webcam.set({
@@ -30,4 +37,18 @@ Webcam.set({
      png_quality:90
 });
 camera= document.getElementById("camera");
+
+function take_snapshot() {
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML= '<img id="selfie_image" src="'+data_uri+'">'
+    });
+
+}
+
+function save() {
+    link=document.getElementById("link");
+    image=document.getElementById("selfie_image").src;
+    link.href= image;
+    link.click();
+}
 
